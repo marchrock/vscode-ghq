bbaa5493ff435b53db3abdfef923671f12ddecb8
marchrock <m4rchrock@gmail.com>
2016-12-23 13:44:44 +0900

    add move to repository command

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,49 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as childProcess from 'child_process';
+import * as sh from 'shelljs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "vscode-ghq" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('extension.ghqMove', ghqMove));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function ghqMove() {
+    let isGhqAvailable = sh.which('ghq');
+    if (!isGhqAvailable) {
+        vscode.window.showWarningMessage('ghq is not installed.');
+        return;
+    }
+
+    let ghqRoot = childProcess.execSync('ghq root').toString().trim();
+
+    childProcess.exec('ghq list', (err, stdout, stderr) => {
+        if (err) {
+            vscode.window.showInformationMessage(err.name + ': ' + err.message);
+        }
+
+        let ghqReposList = stdout.split("\n");
+        vscode.window.showQuickPick(ghqReposList).then(
+            function(selectedRepository) {
+                if (selectedRepository === undefined || selectedRepository === "") {
+                    return;
+                } else {
+                    let uri = vscode.Uri.parse('file://' + ghqRoot + '/' + selectedRepository);
+                    if (fs.existsSync(uri.fsPath)){
+                        let success = vscode.commands.executeCommand('vscode.openFolder', uri);
+                    }
+                }
+            },
+            function(reason) {
+                vscode.window.showWarningMessage(reason.toString())
+            });
+    });
 }
